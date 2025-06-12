337e73442c91c83c48e467ba9e1c369a75841964
MuhammadRehan779e <mdrehan821028@gmail.com>
2025-06-12 20:33:51 +0530

    last error

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 import pickle
 import numpy as np
 app = Flask(__name__)
@@ -36,4 +37,6 @@ with open('house_price_prediction.pkl', 'rb') as f:
         return render_template('index.html', prediction_text=f"prediction Price: {output}")
     
     if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
         app.run(debug=True)
